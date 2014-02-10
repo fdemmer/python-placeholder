730b3f8259748d3a8be1709630152bacc8d793a6
Visgean Skeloru <visgean@gmail.com>
2014-02-10 11:03:32 +0100

    installation details

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-placeholder',
-    version='0.1.1',
+    version='0.1.3',
     description='Placeholder is a simple little python module that creates (drumroll..) placeholder images, based on work of Martin Marcher',
     author="Visgean Skeloru",
     url='https://github.com/Visgean/python-placeholder',
