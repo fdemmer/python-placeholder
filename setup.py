c840366ef4f4d8c59f89c163d7463cec440752b4
Visgean Skeloru <visgean@gmail.com>
2014-04-29 00:37:01 +0200

    0.2 because of last merge.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-placeholder',
-    version='0.1.3',
+    version='0.2',
     description='Placeholder is a simple little python module that creates (drumroll..) placeholder images, based on work of Martin Marcher',
     author="Visgean Skeloru",
     url='https://github.com/Visgean/python-placeholder',
